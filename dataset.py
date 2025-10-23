cde1a1153db21318aa118f092263115046921522
sacardonaf <sacardonaf@unal.edu.co>
2025-10-23 18:07:37 -0500

    cambio enlace

--- dataset.py
+++ dataset.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df=pd.read_csv('files/spotify_churn_dataset.csv')
+df=pd.read_csv('https://raw.githubusercontent.com/StivenSaldarriagaMayorga/Trabajo-IA-2/refs/heads/main/files/spotify_churn_dataset.csv')
 df=df.set_index('user_id')
 
 print(df)
