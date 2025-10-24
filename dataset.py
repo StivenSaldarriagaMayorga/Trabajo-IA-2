d7b5380558193e3d7690e97e2b3941bb8212bb2a
sacardonaf <sacardonaf@unal.edu.co>
2025-10-23 19:02:03 -0500

    normalizacion

--- dataset.py
+++ dataset.py
@@ -1,4 +1,8 @@
+#!pip install scikit-learn
+
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 df=pd.read_csv('https://raw.githubusercontent.com/StivenSaldarriagaMayorga/Trabajo-IA-2/refs/heads/main/files/spotify_churn_dataset.csv')
 
@@ -26,9 +30,26 @@ CC['churn_plan_class']=CC['churn_plan_class'].map({'Free_Churn':0,'Free_NoChurn'
 
 
 #Dividir el conjunto de datos
-X = CC[['gender','age','country','listening_time','songs_played_per_day','skip_rate','device_type','ads_listened_per_week','offline_listening']]
-y = CC['churn_plan_class']
+X1 = CC[['gender','age','country','listening_time','songs_played_per_day','skip_rate','device_type','ads_listened_per_week','offline_listening']]
+y1 = CC['churn_plan_class']
 
-print(X)
-print(y)
+
+#Tamaño de test_set
+X_train_1,X_test_1,y_train_1,y_test_1=train_test_split(X1,y1,test_size=0.25,random_state=seed)
+
+#ED:
+#Dividir el conjunto de datos
+X5 = CC[['gender','age','country','listening_time','songs_played_per_day','skip_rate','device_type','ads_listened_per_week','offline_listening']]
+y5 = CC['churn_plan_class']
+
+#Tamaño de test_set
+X_train_5,X_test_5,y_train_5,y_test_5=train_test_split(X5,y5,test_size=0.25,random_state=seed)
+
+#Escalamiento de los datos
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train_5)
+X_test_scaled = scaler.transform(X_test_5)
+
+print(X_train_scaled)
+print(X_test_scaled)
 
