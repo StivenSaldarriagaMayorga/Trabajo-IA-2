388de875de19ed7580afaacd40a00a42d48e2438
alariaso <141962262+alariaso@users.noreply.github.com>
2025-10-30 19:47:52 -0500

    poner zero_division=np.nan en precision_score para que no incluya esos valores (con divisiones por cero) en el promedio

--- svm.py
+++ svm.py
@@ -1,4 +1,5 @@
-from dataset import dataframes
+from prueba import dataframes
+import numpy as np
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score, f1_score, precision_score
 from sklearn.multiclass import OneVsRestClassifier, OneVsOneClassifier
@@ -11,13 +12,13 @@ def entrenar_y_evaluar(datos, classifier, kernel, *, C, **kwargs):
     y_pred = modelo.predict(X_test)
 
     accuracy = accuracy_score(y_test, y_pred)
-    precision = precision_score(y_test, y_pred, average="weighted")
+    precision = precision_score(y_test, y_pred, average="weighted", zero_division=np.nan)
     f1 = f1_score(y_test, y_pred, average="weighted")
     return { "accuracy": accuracy, "precision": precision, "f1": f1 }
 
 
 for idx, datos in enumerate(dataframes):
-    print(f"====== Dataframe {idx} ======")
+    print(f"====== Caso {idx+1} ======")
     rbf_ovr = entrenar_y_evaluar(datos, OneVsRestClassifier, "rbf", C=1.0, gamma="auto")
     print("RBF OvR:", rbf_ovr)
     rbf_ovo = entrenar_y_evaluar(datos, OneVsOneClassifier, "rbf", C=1.0, gamma="auto")
